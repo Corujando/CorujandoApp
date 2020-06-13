5f41a2e5380c4c7f0e9c2c84cd26abdd7bbc1a7f
alexiadorneles <lecavsalexia@gmail.com>
2020-06-13 16:20:24 -0300

    feat(format): format files.

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,24 @@ import './App.scss'
 import { CRMap, MarkerComponent } from './components/generics/CRMap'
 
 function App() {
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<CRMap
-					zoom={12}
-					MarkerComponent={LocalShipping as MarkerComponent}
-					marker={{
-						lat: -30.056,
-						lng: -51.1622,
-						htmlColor: 'black',
-						style: {
-							fontSize: '50',
-						},
-					}}
-				/>
-			</header>
-		</div>
-	)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <CRMap
+          zoom={12}
+          MarkerComponent={LocalShipping as MarkerComponent}
+          marker={{
+            lat: -30.056,
+            lng: -51.1622,
+            htmlColor: 'black',
+            style: {
+              fontSize: '50',
+            },
+          }}
+        />
+      </header>
+    </div>
+  )
 }
 
 export default App
