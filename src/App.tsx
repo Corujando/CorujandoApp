20122129cd70effd8e158cd231888c2b07c478a8
Joao Pedro Silveira <jpedross1999@gmail.com>
2020-06-13 18:51:05 -0300

    Fix(ESLint): ignorar mudança de versão

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import Paths from './config/Paths'
-import 'firebase/auth'
-import { FirebaseAuthProvider } from '@react-firebase/auth'
-import * as firebase from 'firebase/app'
 import firebaseConfig from './config/FirebaseConfig'
 import Login from './components/screens/Login'
-import './App.scss'
 import { Home } from './components/screens/Home/Home'
+import { FirebaseAuthProvider } from '@react-firebase/auth'
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import './App.scss'
 
 function App() {
   return (
