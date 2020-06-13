5b792b6bef66a62cda10f27b2e7943206c022a8e
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-06-13 15:57:23 -0300

    feat(Objects): mapping db objects

---
+++ src/model/index.tsx
@@ -0,0 +1,4 @@
+import { Break } from './break'
+import { Place } from './place'
+import { Trip } from './trip'
+import { User } from './user'
---
+++ src/model/trip.tsx
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+
+export interface Trip {
+    id: string,
+    userId: string,
+    distance: number,
+    finalLocation: firebase.firestore.GeoPoint,
+    initialLocation: firebase.firestore.GeoPoint,
+    finalTime: firebase.firestore.Timestamp,
+    initialTime: firebase.firestore.Timestamp,
+    time: string,
+    status: string
+}
