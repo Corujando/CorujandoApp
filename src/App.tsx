aa67abbb1740e1c7b6c415c3dd295c930a0705e1
alexiadorneles <lecavsalexia@gmail.com>
2020-06-13 17:19:10 -0300

    feat(loading): create loading and call it in app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import { LocalShipping } from '@material-ui/icons'
 import React from 'react'
 import './App.scss'
-import { CRMap, MarkerComponent } from './components/generics/CRMap'
+import { Loading } from './components/screens/Loading/Loading'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <CRMap
-          zoom={12}
-          MarkerComponent={LocalShipping as MarkerComponent}
-          marker={{
-            lat: -30.056,
-            lng: -51.1622,
-            htmlColor: 'black',
-            style: {
-              fontSize: '50',
-            },
-          }}
-        />
-      </header>
+      <header className="App-header" />
+      {/* <CRMap
+        zoom={12}
+        MarkerComponent={LocalShipping as MarkerComponent}
+        marker={{
+          lat: -30.056,
+          lng: -51.1622,
+          htmlColor: 'black',
+          style: {
+            fontSize: '50',
+          },
+        }}
+      /> */}
+      <Loading />
     </div>
   )
 }
---
+++ src/components/screens/Loading/Loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CRLogo } from '../../generics/CRLogo/CRLogo'
+import './Loading.scss'
+
+export function Loading() {
+  return (
+    <div className="Initial">
+      <CRLogo />
+      {/* <img src={logo} alt="Logo" />
+      <p className="Initial__text">CORUJANDO</p> */}
+    </div>
+  )
+}
