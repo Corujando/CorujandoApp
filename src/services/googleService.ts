e6f506e2d2d71dbf3e4071c490df233f73493e9c
alexiadorneles <lecavsalexia@gmail.com>
2020-06-14 03:05:21 -0300

    feat(google): create route for hospital.

---
+++ src/services/googleService.ts
@@ -0,0 +1,52 @@
+import { Coords } from 'google-map-react'
+
+let map: google.maps.Map<HTMLElement>
+
+class GoogleService {
+  public setMap(mapParam: google.maps.Map<HTMLElement>): void {
+    map = mapParam
+  }
+
+  public createRouteForClosestHospital() {
+    this.findClosestHospital(this.createDirectionsFor)
+  }
+
+  private createDirectionsFor(coord: Coords) {
+    const directionRequest = {
+      destination: coord,
+      origin: map.getCenter(),
+      travelMode: google.maps.TravelMode.DRIVING,
+      drivingOptions: {
+        departureTime: new Date(),
+      },
+    }
+    const service = new google.maps.DirectionsService()
+    const render = new google.maps.DirectionsRenderer({ markerOptions: { visible: false } })
+    service.route(directionRequest, directionResult => {
+      render.setMap(map)
+      render.setDirections(directionResult)
+    })
+  }
+
+  private findClosestHospital(callback: (coord: Coords) => void) {
+    const request: google.maps.places.PlaceSearchRequest = {
+      openNow: true,
+      rankBy: google.maps.places.RankBy.DISTANCE,
+      location: map.getCenter(),
+      type: 'hospital',
+    }
+
+    const service = new google.maps.places.PlacesService(map)
+
+    service.nearbySearch(request, (results, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        const healthOnes = results.filter(r => r.types && r.types.includes('hospital'))
+        const closest = healthOnes[0].geometry!!.location
+        const coords = { lat: closest.lat(), lng: closest.lng() }
+        callback(coords)
+      }
+    })
+  }
+}
+
+export const googleService = new GoogleService()
