82f82339f7008b7a4e2920dd29e471c70467b4a5
alexiadorneles <lecavsalexia@gmail.com>
2020-06-14 00:51:00 -0300

    feat(destiny): create stripped text.

--- src/components/screens/Destino/Destino.tsx
+++ src/components/screens/Destino/Destino.tsx
@@ -5,14 +5,19 @@ import './Destino.scss'
 import { CRPlacesInput } from '../../generics/CRPlacesInput/CRPlacesInput'
 import { CRFooter } from '../../generics/CRFooter/CRFooter'
 
+const UF_GOOGLE_ID = 'administrative_area_level_1'
+
 export function Destino() {
   const [inputValue, setInputValue] = useState('')
+  const [strippedText, setStrippedText] = useState(inputValue)
 
   function onPlaceSelected(
     geocoded: google.maps.GeocoderResult,
     prediction: google.maps.places.AutocompletePrediction,
   ): void {
     setInputValue(prediction.description)
+    const uf = geocoded.address_components.find(c => c.types.includes(UF_GOOGLE_ID))!!.short_name
+    setStrippedText(`${prediction.structured_formatting.main_text} - ${uf}`)
   }
 
   return (
