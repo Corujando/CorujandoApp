3b4a1b2212e67d387e19983b219055abec45a9f1
Leonardo Silveira Berlatto <leoberlatto@gmail.com>
2020-06-14 18:41:23 -0300

    feat(badges-from-firebase): getting badges from firebase

---
+++ src/services/badge.service.ts
@@ -0,0 +1,26 @@
+import { Badge } from '../model/badge'
+import { BaseService } from './base.service'
+
+export class BadgeService extends BaseService {
+    PATH = 'badge'
+
+    async getAllBadges() {
+        const { empty, docs } = await this.getAll();
+        if (empty) {
+            return null
+        }
+
+        return docs.map((doc) => {
+
+            const data = doc.data()
+
+            return {
+                id: doc.id,
+                title: data.title,
+                description: data.description,
+                imageUrl: data.imageUrl
+            } as Badge
+        });
+    }
+
+}
